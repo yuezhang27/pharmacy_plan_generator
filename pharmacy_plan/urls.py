a7802b33febc94a1139f6b63847812fa5ad52c31
yuezhang27 <yzhanged@gmail.com>
2026-02-08 17:27:13 -0800

    urls

--- pharmacy_plan/urls.py
+++ pharmacy_plan/urls.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from careplan import views
 
+# Django是一个Python Web框架
+# urls.py ： 这里放django框架下的所有path（采用RESTful API模式）
+# 格式为：
+"""
+- urlpatterns = [path(), path()...] -> 这里放所有path
+- path的格式：path(路径， 对应的views里的方法， name)
+- 这个path里的name：用于 URL 反向解析（reverse() / {% url %}）
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
